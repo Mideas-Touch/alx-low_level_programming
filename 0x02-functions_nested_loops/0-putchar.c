3300551d3c0a95114e1fb80fd92077cbcb3a039c
flipblip <wickynjy@gmail.com>
2023-03-22 01:49:17 -0700

    Task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,19 @@
-#include
+#include "main.h"
+/**
+ * main - prints the string "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+		{
+		_putchar(p[i]);
+		}
+		_putchar('\n');
+	return (0);
+}
