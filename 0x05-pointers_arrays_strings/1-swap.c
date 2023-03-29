b8a556f8e5d3512986e3869d973581a19fdfa9b0
flipblip <wickynjy@gmail.com>
2023-03-29 07:22:46 -0700

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+
+
+#include "main.h"
+
+/**
+ * swap_int - swaps values of two pointers
+ * @a: function parameter 1
+ * @b: function parameter 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
