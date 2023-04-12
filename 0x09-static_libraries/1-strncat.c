515848668f98870d72e911acfd050f0870c723ac
flipblip <wickynjy@gmail.com>
2023-04-12 02:03:01 -0700

    cat two strings

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -1,24 +1,22 @@
 #include "main.h"
 /**
- * *_strncat - concatenates n bytes of two strings
- * @dest: pointer destination
- * @src: pointer source
- * @n: number of bytes
- * Return: void
-*/
+ * *_strncat - concantenates two strings
+ * @src: source string
+ * @dest: destiny string
+ * @n: maximum number of characters to append
+ * Return: dest
+ */
 
 char *_strncat(char *dest, char *src, int n)
+
 {
 
-int dest_len, i;
-for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
-;
+	int index = 0, dest_len = 0;
 
-for (i = 0; i < n && src[i] != '\0'; i++)
-	dest[dest_len + i] = src[i];
+	while (dest[index++])/* loop interates inside dest string until null*/
+	dest_len++;
+	for (index = 0; src[index] && index < n; index++)/* appended char < n*/
+	dest[dest_len++] = src[index];
+	return (dest);
 
-/*should end with a end of string char*/
-dest[dest_len + i] = '\0';
-
-return (dest);
 }
