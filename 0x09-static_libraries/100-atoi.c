fae3ec7e59fbf5bba670a69cfc0bd6e419445221
flipblip <wickynjy@gmail.com>
2023-04-12 01:54:57 -0700

    return str as integer

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,37 @@
+
+#include "main.h"
+
+/**
+ * _atoi - convert a string to an integer.
+ * @s: the string to convert.
+ * Return: intger.
+ */
+
+int _atoi(char *s)
+{
+	int i = 0;
+	int x = 1;
+	int y = 0;
+	unsigned int j = 0;
+
+	while (s[i])
+	{
+		if (s[i] == '-')
+		{
+			x *= -1;
+		}
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			y = 1;
+			j = (j * 10) + (s[i] - '0');
+			i++;
+		}
+		if (y == 1)
+		{
+			break;
+		}
+		i++;
+	}
+	j *= x;
+	return (j);
+}
