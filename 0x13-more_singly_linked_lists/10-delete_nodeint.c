570e1b2a6f166e80a5c89fe0a04c8ad62e2d7dae
flipblip <wickynjy@gmail.com>
2023-04-29 10:35:15 -0700

    delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -12,7 +12,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	unsigned int i;
 	listint_t *current, *subsequent = *head;
 
-	if (!head || !*head)
+	if (subsequent == NULL)
 		return (-1);
 	current = *head;
 	if (index == 0)
